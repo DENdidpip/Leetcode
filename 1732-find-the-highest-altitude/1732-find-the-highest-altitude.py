6975609d280ebd9a0d85853ef224a25c2622e2a8
Danylo Breskyi <161898758+DENdidpip@users.noreply.github.com>
2026-08-31 01:35:13 +0200

    Time: 4 ms (7.62%), Space: 12.4 MB (19.59%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def largestAltitude(self, gain):
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+        res = 0
+        curr = 0
+        for i in range(len(gain)):
+            curr += gain[i]
+            res = max(res, curr)
+        return res
